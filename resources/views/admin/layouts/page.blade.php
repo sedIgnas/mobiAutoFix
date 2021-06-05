cb63933768ea6146e5de3106819d0ac2e3ab05ae
IgnasSeduikis <ignas.seduikis@gmail.com>
2021-06-05 19:57:01 +0300

    removed navbar, fixed sidebar links, fixed footer

--- resources/views/admin/layouts/page.blade.php
+++ resources/views/admin/layouts/page.blade.php
@@ -19,6 +19,7 @@
                     <div class="row mb-2">
                         <div class="col-sm-6">
                             <h1 class="m-0">{{$content_title ?? 'Welcome'}}</h1>
+
                         </div><!-- /.col -->
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
--- resources/views/admin/layouts/partials/sidebar.blade.php
+++ resources/views/admin/layouts/partials/sidebar.blade.php
@@ -2,37 +2,18 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="https://laravel.com/" class="brand-link">
-        <img src="{{asset('assets/admin/images/logo.png')}}" alt="logo" class="brand-image elevation-3"
-             style="opacity: .8">
-        <span class="brand-text font-weight-light">@lang('app.programming_courses')</span>
+        <span class="brand-text font-weight-light">@lang('Control panel')</span>
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-                <img src="/storage/uploads/images/thumb/{{Auth::user()->image ?? ''}}" style="width:4rem" class="img-circle elevation-1" alt="User Image">
-            </div>
             <div class="info">
-{{--                <a href="#" class="d-block">{{Auth::user() ? Auth::user()->getFullNameAttribute() : ''}}</a>--}}
+            <span class="text-light" class="h5">Logged in as: </span>
+                <a href="#" class="d-block">{{Auth::user() ? Auth::user()->getFullNameAttribute() : ''}}</a>
             </div>
         </div>
-
-        <!-- SidebarSearch Form -->
-{{--        <div class="form-inline">--}}
-{{--            <div class="input-group" data-widget="sidebar-search">--}}
-{{--                <input class="form-control form-control-sidebar" type="search" placeholder="Search"--}}
-{{--                       aria-label="Search">--}}
-{{--                <div class="input-group-append">--}}
-{{--                    <button class="btn btn-sidebar">--}}
-{{--                        <i class="fas fa-search fa-fw"></i>--}}
-{{--                    </button>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-
-        <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                 data-accordion="false">
@@ -83,14 +64,6 @@
                         </p>
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a href="{{route('admin.make.index')}}" class="nav-link">
-                        <i class="nav-icon fas fa-map-marker"></i>
-                        <p>
-                            @lang('app.locations')
-                        </p>
-                    </a>
-                </li>
                 <!-- logout --><li class="nav-item">
                     <form id="logout" action="{{ route('logout') }}" method="POST"
                           style="display: none;">
@@ -99,7 +72,7 @@
                     <a href="#logout" onclick="event.preventDefault();document.getElementById('logout').submit()" class="nav-link">
                         <i class="nav-icon fas fa-sign-out-alt"></i>
                         <p>
-                            @lang('app.logout')
+                            @lang('Log out')
                         </p>
                     </a>
                 </li>
